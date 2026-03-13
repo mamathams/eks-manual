10e3bd9d807d62a452c01f618047a72390fee959
mamata <mamatha>
2026-03-13 13:36:50 +0530

    Tag subnets for EKS

--- main.tf
+++ main.tf
@@ -29,6 +29,12 @@ resource "aws_subnet" "public_1" {
   cidr_block              = var.public_subnet_cidrs[0]
   availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name                                        = "${var.cluster_name}-public-1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
 }
 
 resource "aws_subnet" "public_2" {
@@ -36,6 +42,12 @@ resource "aws_subnet" "public_2" {
   cidr_block              = var.public_subnet_cidrs[1]
   availability_zone       = "${var.region}b"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name                                        = "${var.cluster_name}-public-2"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
 }
 
 ########################
@@ -46,12 +58,24 @@ resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = var.private_subnet_cidrs[0]
   availability_zone = "${var.region}a"
+
+  tags = {
+    Name                                        = "${var.cluster_name}-private-1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
 }
 
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = var.private_subnet_cidrs[1]
   availability_zone = "${var.region}b"
+
+  tags = {
+    Name                                        = "${var.cluster_name}-private-2"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
 }
 
 ########################
